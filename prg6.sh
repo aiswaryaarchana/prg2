27841b38f0bcef470b7fa4ad998f5598582412a7
aiswaryaarchana <aiswaryamohan5050>
2023-01-18 16:06:11 +0530

    seventh commit

---
+++ prg6.sh
@@ -0,0 +1,19 @@
+echo "enter 3 numbers"
+read a 
+read b
+read c
+if [ $a -gt $b ]
+then
+if [$a -gt $c]
+then
+echo "largest is $a"
+else
+echo "largest is $c"
+fi
+elif [ $b -gt $c ]
+then
+echo "largest is $b"
+else
+echo "largest is $c"
+fi
+ 
